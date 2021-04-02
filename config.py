b2117f564331b4192ad953cb2fa96c3d8702a149
Tim <59373582+timseifer@users.noreply.github.com>
2021-04-02 15:48:36 -0400

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-api_key = "0dewEHx5BMgnKRwdMIuYXIqgx"
-api_secret = "RYllxqJiOjwLbrbYbCJ2eIlOeap3KPyfUtBoUS5dKz3hdFcTe6"
-access_token = "1377475999638556673-duS2Lv2FaAGo2A5BYmgtgqmPGenIiX"
-token_secret = "rKGgFqo7cymCo9IdYYlxQQd5HfeFwe8tNe4EvjTn6u6Ie"
